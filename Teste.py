bcd4c3a7a358c85fce4e449a6cfd9e4061b5e8dd
Akageluv <akageluv@gmail.com>
2022-08-04 22:28:21 -0300

    dois

--- Teste.py
+++ Teste.py
@@ -1,2 +1,15 @@
-nome = input  ('qual é seu nome:')
-print ('seja bem vindo', nome)
+print("Olá, vou lhe fazer uma pergunta")
+
+
+while True :
+    resposta = input ("como você está? ")
+    if resposta == "bem":
+        print ("Que bom, continue assim")
+        break
+
+    elif resposta == "mal":
+        print ("Não desanime")
+        break
+    
+    print
+    
