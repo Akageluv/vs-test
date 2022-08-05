190896360c131be3923813d4b839771c7e247551
Akageluv <akageluv@gmail.com>
2022-08-05 10:42:05 -0300

    Atualização menu 2

--- Teste.py
+++ Teste.py
@@ -1,4 +1,5 @@
 #Pergunta ao cliente
+
 print("Olá, vou lhe fazer uma pergunta")
 
 #Sistema wile de repetição
@@ -17,19 +18,20 @@ while True :
 
 
     #if (Se) a resposta for bem, mostrrar "que bom continue assim"
-    if resposta == "bem":
+    if resposta == "bem" or resposta == "Bem" or resposta == "ótimo" or resposta == "ok":
     
         print ("Que bom, continue assim")
         break
-    #Elif faz parte da lógica do if, como se fosse um "Se não" dentro do if.
-    elif resposta == "mal":
 
+    #Elif faz parte da lógica do if, como se fosse um "Se não" dentro do if.
+    elif resposta == "mal" or resposta == "Mal" or respoosta == "Ruim" or resposta == "ruim":
+        
         print ("Não desanime!")
         break
 
         #Break é a finalização do WHILE TRUE
 
-    else: print ("Responda com bem ou mebal")
+    else: print ("Responda com bem ou mal")
     #O else fará com que qualquer coisa respondida sem ser "bem" ou "mal"voltará a mesma pergunta.
 
 
